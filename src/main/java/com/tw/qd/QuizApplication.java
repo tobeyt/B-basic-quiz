ee354287ed0ffe41ce7bff74c8e9b9f2c5d994ee
di.qin <di.qin@thoughtworks.com>
2020-10-21 20:52:35 +0800

    [qd]: refactor: move initCode to application.

--- src/main/java/com/tw/qd/QuizApplication.java
+++ src/main/java/com/tw/qd/QuizApplication.java
@@ -1,13 +1,60 @@
 package com.tw.qd;
 
+import com.tw.qd.dto.Education;
+import com.tw.qd.dto.User;
+import com.tw.qd.service.UserService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizApplication {
+public class QuizApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizApplication.class, args);
-	}
+    private final UserService userService;
 
+    public QuizApplication(UserService userService) {
+        this.userService = userService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        userService.createUser(User.builder()
+                .age(24L)
+                .avatar("https://inews.gtimg.com/newsapp_match/0/3581582328/0")
+                .description("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, non, " +
+                        "dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut " +
+                        "fuga aliquam ad asperiores voluptatem dolorum! Quasi.")
+                .name("KAMIL")
+                .build());
+
+        userService.createEducationByUserId(1L, Education.builder()
+                .year(1990L)
+                .title("I was born in Katowice")
+                .description("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi.")
+                .userId(1L)
+                .build());
+        userService.createEducationByUserId(1L, Education.builder()
+                .year(2005L)
+                .title("Secondary school specializing in artistic")
+                .description("Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus.")
+                .userId(1L)
+                .build());
+        userService.createEducationByUserId(1L, Education.builder()
+                .year(2009L)
+                .title("First level graduation in Graphic Design")
+                .description("Aspernatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? Iusto, quibusdam asperiores unde repellat.")
+                .userId(1L)
+                .build());
+        userService.createEducationByUserId(1L, Education.builder()
+                .year(2012L)
+                .title("Second level graduation in Graphic Design")
+                .description("Ducimus, aliquam tempore autem itaque et accusantium!")
+                .userId(1L)
+                .build());
+    }
 }
--- src/main/java/com/tw/qd/repository/UserRepository.java
+++ src/main/java/com/tw/qd/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.tw.qd.repository;
 
 import com.tw.qd.dto.Education;
 import com.tw.qd.dto.User;
-import lombok.Getter;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -12,51 +11,10 @@ import java.util.stream.Collectors;
 
 @Repository
 public class UserRepository {
-    private Long generatedUserId = 1L;
-    private List<User> users = new ArrayList<User>() {
-        {
-            add(new User(
-                    1L,
-                    "KAMIL",
-                    24L,
-                    "https://inews.gtimg.com/newsapp_match/0/3581582328/0",
-                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, non, " +
-                            "dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut " +
-                            "fuga aliquam ad asperiores voluptatem dolorum! Quasi."
-            ));
-        }
-    };
+    private Long generatedUserId = 0L;
+    private final List<User> users = new ArrayList<>();
 
-    private List<Education> educations = new ArrayList<Education>() {
-        {
-
-            add(new Education(
-                    1L,
-                    1990L,
-                    "I was born in Katowice",
-                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, exercitationem, totam, dolores iste dolore est aut modi."
-            ));
-            add(new Education(
-                    1L,
-                    2005L,
-                    "Secondary school specializing in artistic",
-                    "Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur ipsum ducimus quibusdam quis voluptatibus."
-            ));
-            add(new Education(
-                    1L,
-                    2009L,
-                    "First level graduation in Graphic Design",
-                    "Aspernatur, mollitia, quos maxime eius suscipit sed beatae ducimus quaerat quibusdam perferendis? Iusto, quibusdam asperiores unde repellat."
-            ));
-            add(new Education(
-                    1L,
-                    2012L,
-                    "Second level graduation in Graphic Design",
-                    "Ducimus, aliquam tempore autem itaque et accusantium!"
-            ));
-
-        }
-    };
+    private final List<Education> educations = new ArrayList<>();
 
     public Optional<User> getUserById(Long id) {
         return users.stream().filter(user -> user.getId().equals(id)).findFirst();
