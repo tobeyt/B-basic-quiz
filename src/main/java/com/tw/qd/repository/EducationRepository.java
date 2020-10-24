f5864e1b0e7707fec8f68281abb21132e5db2345
di.qin <di.qin@thoughtworks.com>
2020-10-24 13:19:04 +0800

    [qd]: finish jpa.

--- src/main/java/com/tw/qd/repository/EducationRepository.java
+++ src/main/java/com/tw/qd/repository/EducationRepository.java
@@ -2,9 +2,11 @@ package com.tw.qd.repository;
 
 import com.tw.qd.entity.EducationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EducationRepository extends JpaRepository<EducationEntity, Long> {
     List<EducationEntity> findAllByUserEntityId(long userId);
 }
